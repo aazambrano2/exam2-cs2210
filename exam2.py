bfbd1f350d1bede9a986ee891898347e860c8d64
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-25 12:21:46 -0700

    added function stubs to set up the problems

---
+++ exam2.py
@@ -0,0 +1,35 @@
+
+
+def p1():
+    pass
+
+def p2():
+    pass
+
+def p3():
+    pass
+
+def p4():
+    pass
+
+def p5():
+    pass
+
+def p6():
+    pass
+
+def p7():
+    pass
+
+def p8():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print()
